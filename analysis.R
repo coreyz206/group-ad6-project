643ec0b25ff34c584826ad3f1ee0417359410aab
Ian O'Brien <iano3@uw.edu>
2019-03-09 14:41:04 -0800

    changed url

--- analysis.R
+++ analysis.R
@@ -2,7 +2,7 @@
 library("ZillowR")
 library("httr")
 library("jsonlite")
-library("plyr")
+library("plyr")   
 library("dplyr")
 library("XML")
 
@@ -16,14 +16,15 @@ demographics_endpoint <- "GetDemographics.htm"
 
 response_zillow <- GET(paste0(base_uri, neighborhood_endpoint, "zws-id=", zwsid, "&state=", "wa", "&city=seattle"))
 
-response_test <- GET("http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1gy3bbhi1hn_211k7&state=wa&city=seattle&childtype=neighborhood")
-#response_chart <- GET("http://www.zillow.com/webservice/GetRegionChart.htm?zws-id=X1-ZWz1gy3bbhi1hn_211k7&state=wa&city=seattle&childtype=neighborhood")
+response_test <- GET(paste0("http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=", zwsid, "&state=wa&city=seattle&childtype=neighborhood"))
+#response_chart <- GET("http://www.zillow.com/webservice/GetRegionChart.htm?zws-id=<zwsid&state=wa&city=seattle&childtype=neighborhood")
 response_zillow_text <- content(response_zillow, "text")
 response_zillow_data <- xmlParse("GetRegionChildren.xml")
 
-
 response_zillow_text
 response_zillow_data
+
+as.data.frame(response_zillow_data)
 
 
 
