64f835a5552ca0170cf33e5fc85edc9c9efb62bd
Ian O'Brien <iano3@uw.edu>
2019-03-11 15:33:12 -0700

    add main panel to ui

--- my_ui.R
+++ my_ui.R
@@ -7,6 +7,15 @@ my_ui <- fluidPage(
     sidebarPanel(
       selectInput(inputId = "year", label = "Select a Year", 
                   choices = year_choices, selected = "2014")
+    ),
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        #tabPanel("Map", plotOutput(outputId = "urban_pop_co2"), textOutput(outputId = "map_text")),
+        tabPanel("Table", dataTableOutput(outputId = "datasets_by_year"))
+      )
     )
   )
 )
+
+
