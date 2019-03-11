41171a8161d69942b8769a5fc3a7413224cf162d
Ian O'Brien <iano3@uw.edu>
2019-03-10 19:40:02 -0700

    add world map

--- analysis.R
+++ analysis.R
@@ -1,6 +1,8 @@
 ### PLACEHOLDER ###
 library("dplyr")
 library("ggplot2")
+library("maps")
+library("mapproj")
 
 maternal_mortality_rate <- read.csv("data/maternal_mortality_filtered.csv", stringsAsFactors = FALSE)
 life_expectancy_at_birth <- read.csv("data/life_expectancy_at_birth_filtered.csv", stringsAsFactors = FALSE)
@@ -10,4 +12,8 @@ unemployment_percent <- read.csv("data/unemployment_percent_filtered.csv", strin
 co2_emissions <- read.csv("data/co2_emissions_filtered.csv", stringsAsFactors = FALSE)
 urban_population <- read.csv("data/urban_population_percent_filtered.csv", stringsAsFactors = FALSE)
 
-
+world_map <- map_data("world")
+world_map_updated <- mutate(
+  world_map,
+  Country.Code == iso.alpha(world_map$region, n = 3)
+)
