6b71ba8ef0b7b83e7d3cae0f6c025ec0255f51a2
Ian O'Brien <iano3@uw.edu>
2019-03-13 11:53:44 -0700

    cleaned the unneeded commented sections

--- analysis.R
+++ analysis.R
@@ -1,13 +1,5 @@
 ### analysis and data prep
 
-#https://data.worldbank.org/indicator/NY.GDP.PCAP.CD?view=chart
-#https://data.worldbank.org/indicator/SP.URB.TOTL.IN.ZS?view=chart
-#https://data.worldbank.org/indicator/NY.GDP.PCAP.CD?view=chart
-
-#maternal_mortality_rate <- read.csv("data/maternal_mortality_filtered.csv", stringsAsFactors = FALSE)
-#life_expectancy_at_birth <- read.csv("data/life_expectancy_at_birth_filtered.csv", stringsAsFactors = FALSE)
-#income_per_capita <- read.csv("data/income_per_capita_filtered.csv", stringsAsFactors = FALSE)
-#unemployment_percent <- read.csv("data/unemployment_percent_filtered.csv", stringsAsFactors = FALSE)
 gdp_per_capita <- read.csv("data/gdp_per_capita_filtered.csv", stringsAsFactors = FALSE)
 co2_emissions <- read.csv("data/co2_emissions_filtered.csv", stringsAsFactors = FALSE)
 urban_population <- read.csv("data/urban_population_percent_filtered.csv", stringsAsFactors = FALSE)
@@ -63,16 +55,6 @@ all_data_global <- co2_urban_mapdata %>%
   mutate("GDP per capita, in current US$" = gdp_formatted$`GDP per capita, in current US$`) %>%
   select(-SUBREGION, -LON, -LAT)
 
-#what needs to be done:
-##Each of us need to ask a couple of analytical questions that we can then answer using the data?
-###The requirements say that we can't just include visualizations, we should for each question, but we also need to answer the question
-###using our data. 
-
-##QUESTIONS - choice one (maternal, life, income)
-#How does income per capita effect the life expectancy and and maternal mortality rate, is a positive or negative correlation or no correlation?
-#What is the average life expectancy in the world and for each country?
-#Why are maternal mortality rates different between countries, does it tie into income per capita
-#What countries have the highest and lowest life expectancies, what is the average life expectancy?
 
 ##QUESTIONS - choice two (co2 emissions, urban population, gdp per capita)
 #Does the GDP per capita effect the amount of co2 emissions by a country? 
@@ -107,14 +89,6 @@ co2_and_urban_change <- mutate(
 )
 
 all_three <- left_join(co2_and_urban, gdp_per_capita, by = "Country.Name")
-
-##shiny app
-###must be interactive, include visualizations, text must be interactive based on user selections
-###must have links to where we got the information from
-###must have interactive widgets and answer our critical questions
-####optional, use some css to style our shiny app so that it doesn't look so plain, only if time permitting
-##We should try to make an interactive map
-
 
 ## Data Processing for Comparative Column Chart
 
--- my_server.R
+++ my_server.R
@@ -5,30 +5,6 @@ library("ggplot2")
 
 
 my_server <- function(input, output) {
-  # output$intro <- renderText({
-  #   intro_text <- paste("The following application formulates a report on the correlations between GDP per capita, the urban population
-  #   as a percentage of the total population and the CO2 emissions by each country. Under each tab, there are different visualizations
-  #   and comparisons of how these indicators influence each other and how they answer questions about cultures in countries across the
-  #   globe. First, a little bit of background of these indicators is necessary, outlining both what they are and how they are calculated.
-  #   GDP per capita, or Gross Domestic Product, is a measure of a country's economy and how many people are living in that country. It is
-  #   calculated by taking the GDP and then dividing it by the population of that country. So, GDP per capita is a way of putting a number
-  #   to the standard of living in the countries by measuring the amount of people and the countries gross domestic product. For clarification,
-  #   gross domestic product is the total amount of goods and services provided by a country during one fiscal year. As for the urban population
-  #   indicator, it is quite simple, it is the amount the amount of people living in the country divided by the amount of people in the country
-  #   living in an urban area, giving the resulting percentage of people living in urban areas in each country. Lastly, CO2 emissions are
-  #   the amount of carbon dioxide released into the atmosphere. In this case, the CO2 emissions are calculated on the scale of metric tons
-  #   per capita, which means that it is the metric tons of CO2 emissions divided by the total population of each country in order to get a
-  #   relative scale for each country. This way it is based more on how much they are producing relative to how big their population is, rather
-  #   than just the amount of CO2 emissions in total. The following report was created to show the effects of urban living and GDP on the CO2
-  #   emissions from 1990-2016. The data also allows for a look into how industrialization (higher gdp) has impacted urban living and the CO2
-  #   emissions. The following data all comes from the", paste0(a("World Bank Group", href="https://data.worldbank.org/indicator?tab=all"), "."),
-  #   "As each tab is investigated the questions about these indicators and their correlations are answered. The datasets can be found by clicking
-  #   on the following links:", paste0(a("Urban Population", href="https://data.worldbank.org/indicator/SP.URB.TOTL.IN.ZS?view=chart"), ","),
-  #   paste0(a(href="https://data.worldbank.org/indicator/EN.ATM.CO2E.PC?view=chart", "CO2 Emissions"), ","),
-  #   paste0(a("GDP per capita", href="https://data.worldbank.org/indicator/NY.GDP.PCAP.CD?view=chart"), "."))
-  #   intro_text
-  # })
-  
   output$datasets_by_year <- renderDataTable({
     comparison_by_year <- all_three %>% 
       select(
