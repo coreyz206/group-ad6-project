b26f1e83b7759a019cb3dcc74e27cb4d9f4c8b0c
Corey Zarkowski <coreyz206@gmail.com>
2019-03-12 17:09:16 -0700

    finished leaflet data setup

--- analysis.R
+++ analysis.R
@@ -1,18 +1,48 @@
 ### analysis and data prep
 
-maternal_mortality_rate <- read.csv("data/maternal_mortality_filtered.csv", stringsAsFactors = FALSE)
-life_expectancy_at_birth <- read.csv("data/life_expectancy_at_birth_filtered.csv", stringsAsFactors = FALSE)
+#maternal_mortality_rate <- read.csv("data/maternal_mortality_filtered.csv", stringsAsFactors = FALSE)
+#life_expectancy_at_birth <- read.csv("data/life_expectancy_at_birth_filtered.csv", stringsAsFactors = FALSE)
+#income_per_capita <- read.csv("data/income_per_capita_filtered.csv", stringsAsFactors = FALSE)
+#unemployment_percent <- read.csv("data/unemployment_percent_filtered.csv", stringsAsFactors = FALSE)
 gdp_per_capita <- read.csv("data/gdp_per_capita_filtered.csv", stringsAsFactors = FALSE)
-income_per_capita <- read.csv("data/income_per_capita_filtered.csv", stringsAsFactors = FALSE)
-unemployment_percent <- read.csv("data/unemployment_percent_filtered.csv", stringsAsFactors = FALSE)
 co2_emissions <- read.csv("data/co2_emissions_filtered.csv", stringsAsFactors = FALSE)
 urban_population <- read.csv("data/urban_population_percent_filtered.csv", stringsAsFactors = FALSE)
 
-world_map <- map_data("world")
-world_map_updated <- mutate(
-  world_map,
-  Country.Code = iso.alpha(world_map$region, n = 3)
-)
+
+### LEAFLET DATA SETUP ###
+co2_formatted <- co2_emissions %>%
+  gather(key = year, value = "CO2 Emissions (metric tons per capita)", 
+         -Country.Name, -Country.Code, -Indicator.Name, -Indicator.Code) %>%
+  select(-Indicator.Name, -Indicator.Code)
+
+urban_formatted <- urban_population %>%
+  gather(key = year, value = "percentpop", 
+         -Country.Name, -Country.Code, -Indicator.Name, -Indicator.Code) %>%
+  select(-Indicator.Name, -Indicator.Code)
+
+urban_formatted$year <- sapply(urban_formatted$year, substr, 2, 5)
+urban_formatted$year <- as.numeric(urban_formatted$year)
+urban_formatted <- urban_formatted %>%
+  filter(year < 2015)
+
+co2_urban_formatted <- co2_formatted %>%
+  mutate(
+    "Percentage of Population in Urban Areas" = urban_formatted$percentpop
+  )
+co2_urban_formatted$year <- sapply(co2_urban_formatted$year, substr, 2, 5)
+
+colnames(co2_urban_formatted)[1:3] <- c("Country.Name", "Country.Code",
+                                        "Year")
+
+world_map <- readOGR(dsn = "./data", layer = "TM_WORLD_BORDERS-0.3", stringsAsFactors = FALSE)
+
+world_map@data <- select(world_map@data, -FIPS, -ISO2, -UN, -AREA, -POP2005, -NAME)
+colnames(world_map@data)[1] <- "Country.Code"
+
+co2_urban_mapdata <- left_join(co2_urban_formatted, world_map@data, by = "Country.Code")
+world_map@data <- co2_urban_mapdata
+
+### LEAFLET SETUP END
 
 #what needs to be done:
 ##Each of us need to ask a couple of analytical questions that we can then answer using the data?
